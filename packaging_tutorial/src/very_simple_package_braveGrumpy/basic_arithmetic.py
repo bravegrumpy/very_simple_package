78af20b141b8d1bcde0f6415e3df49034391d3c2
Robin Zollner <robin.zollner97@gmail.com>
2023-06-15 11:28:01 -0400

    debugging Arithmetic and adding test directory

--- packaging_tutorial/src/very_simple_package_braveGrumpy/basic_arithmetic.py
+++ packaging_tutorial/src/very_simple_package_braveGrumpy/basic_arithmetic.py
@@ -1,9 +1,12 @@
 class Arithmetic:
     def __init__(self, *args):
-        self.params = list(*args)
+        self.params = list(args)
     
     def add(self):
         return sum(self.params)
+    
+    def sum(self):
+        return self.add()
     
     def multiply(self):
         prod = 1
