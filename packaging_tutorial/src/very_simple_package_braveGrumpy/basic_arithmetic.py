afc224936622149ed0d64d1f06e1fa57ae6d5531
Robin Zollner <robin.zollner97@gmail.com>
2023-06-16 00:43:16 -0400

    adding docstrings to class

--- packaging_tutorial/src/very_simple_package_braveGrumpy/basic_arithmetic.py
+++ packaging_tutorial/src/very_simple_package_braveGrumpy/basic_arithmetic.py
@@ -1,14 +1,19 @@
 class Arithmetic:
+    """This class adds and multiplies."""
     def __init__(self, *args):
+        """The Arithmetic class can accept any number of arguments."""
         self.params = list(args)
     
     def add(self):
+        """Returns the sum of all the arguments passed into the instance."""
         return sum(self.params)
     
     def sum(self):
+        """Does the same thing as add."""
         return self.add()
     
     def multiply(self):
+        """This function finds the product of all the arguments passed inot hte Arithmetic object."""
         prod = 1
         for x in self.params:
             prod = prod * x
